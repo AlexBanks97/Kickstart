c64d830f5175cf0cf17699b45ca2e86328adb70e
AlexBanks97 <alexbanks097@gmail.com>
2017-04-18 20:38:33 +0200

    Test 2

--- Kickstart.bat
+++ Kickstart.bat
@@ -1,8 +1,4 @@
 :: KICKSTART SCRIPT
-
-:: Install Chocolatey
-:: @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
 :: Install Essentials
 choco upgrade chocolatey
 choco install chocolateygui
