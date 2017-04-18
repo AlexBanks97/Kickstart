a3f1afc57873249abc626e20d2460a7f261b8e46
AlexBanks97 <alexbanks097@gmail.com>
2017-04-18 20:29:24 +0200

    small fix

--- Kickstart.bat
+++ Kickstart.bat
@@ -4,6 +4,7 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 :: Install Essentials
+choco upgrade chocolatey
 choco install chocolateygui
 choco install googlechrome
 choco install winrar
