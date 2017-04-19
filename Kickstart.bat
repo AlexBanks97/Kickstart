a319646eb0be70e2e0da487106aaf36673ecf0c5
AlexBanks97 <alexbanks097@gmail.com>
2017-04-19 21:54:08 +0200

    Fix

--- Kickstart.bat
+++ Kickstart.bat
@@ -12,10 +12,8 @@ choco upgrade intellijidea-ultimate -y
 choco install lastpass -y
 choco upgrade lastpass -y
 
-echo Finished installing chocolatey packages... 
+echo "Finished installing chocolatey packages..."
 
 mkdir C:\Development
-
-echo Created directory 'development' on your C drive... && Now cloning Kickstart repo into development...
 
 git clone https://github.com/AlexBanks97/Kickstart.git C:\Development
