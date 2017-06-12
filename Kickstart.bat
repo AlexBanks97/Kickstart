efcd50de531e25a44784e9fdf447a47dd1ef1990
banks <alsb@itu.dk>
2017-06-12 08:22:17 +0200

    removed intellij from kickstart script

--- Kickstart.bat
+++ Kickstart.bat
@@ -3,12 +3,11 @@ REM KICKSTART SCRIPT
 REM Install Essentials
 choco upgrade chocolatey -y
 choco install googlechrome -y
+choco upgrade googlechrome -y
 choco install winrar -y
 choco install jdk8 -y
 choco install git -y
 choco upgrade git -y
-choco install intellijidea-ultimate -y
-choco upgrade intellijidea-ultimate -y
 
 mkdir C:\Development
 
